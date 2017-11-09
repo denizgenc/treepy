9e406789ef3abf7350ba4c659650d4e21c7aa790
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2017-11-09 14:26:43 +0000

    Updated treepy.py
    A new version edited from the previous, using nano on my phone, so it might not look very neat.
    This adds the capability to give the directory you want to run the tree in as an argument to the script. I could not access external storage via Termux without using `su`, but the superuser in Termux did not have the python binaries; necessitating this workaround.

--- treepy.py
+++ treepy.py
@@ -4,7 +4,7 @@
 # writes the same tree it outputs to a file called TREE.txt
 # ... because for some reason just like... piping it through into a file doesn't work.
 # text encoding issues, lmao
-import os, os.path
+import os, os.path, sys
 
 def listdir(path="."):
     '''
@@ -20,7 +20,7 @@ def listdir(path="."):
                 list.append([entry.name, entry.path])
     list.sort(key=lambda element: element[0].lower()) # sorts by the directory name only
     return list
-    
+
 excludepath = os.path.join(".","EXCLUDE.txt")
 if os.path.exists(excludepath):
     excludefile = open(excludepath)
@@ -32,14 +32,27 @@ treefile = open(treefilepath, "w", encoding="utf8")
 def treeloop(cwd, offset, treefile):
     heading = "|- "
     for dir in cwd:
-        if not (dir[0] in excludelist):
+        if os.path.exists(excludepath):
+            if not (dir[0] in excludelist):
+                print("".join([" "*offset,heading,dir[0]]))
+                treefile.write("".join([" "*offset,heading,dir[0],"\n"]))
+                subdir = listdir(dir[1])
+                if subdir != []:
+                    treeloop(subdir, offset + 1, treefile)
+        else:
             print("".join([" "*offset,heading,dir[0]]))
             treefile.write("".join([" "*offset,heading,dir[0],"\n"]))
             subdir = listdir(dir[1])
             if subdir != []:
                 treeloop(subdir, offset + 1, treefile)
 
-topdir = listdir(os.path.abspath("."))
+#topdir @
+if len(sys.argv) == 1:
+    topdir = listdir(os.path.abspath("."))
+else:
+    topdir = listdir(sys.argv[1])
 offset = 0 # this will offset the heading
-                
+
+#print("printing sys.argv 0 and 1")
+#print(sys.argv[0], sys.argv[1])
 treeloop(topdir, offset, treefile)
